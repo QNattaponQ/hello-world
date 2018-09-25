694000438b7f41cc9c3694cf10a602beb8f3a276
QNattaponQ <sunshiro9@gmail.com>
2018-09-25 15:19:58 +0700

    test rabbit mq

--- rabbitmq/receive.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-)
-
-func receive() {
-	fmt.Println("Testing recievedddd")
-}
--- rabbitmq/send/send.go
+++ rabbitmq/send/send.go
@@ -25,4 +25,26 @@ func main() {
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
 }
